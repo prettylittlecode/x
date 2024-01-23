203b93436238da5d5ca7a134bc2c3ab7f537b431
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-23 12:36:33 +1200

    Create SinglePage.jsx

---
+++ frontend/src/pages/postpage/SinglePage.jsx
@@ -0,0 +1,31 @@
+import Feed from "../../components/Home/Feed/Feed";
+import Sidebar from "../../components/Home/Sidebar/Sidebar";
+import Widgets from "../../components/Home/Widgets/Widgets";
+import { useNavigate } from "react-router-dom";
+import "../home/Home.css";
+import { Profile } from "../../components/Home/Profile/Profile";
+import { modelState, ProfileState,TweetPageState } from "../../recoil/modelAtom";
+import { useRecoilState } from "recoil";
+import { CommentModel } from "../../components/Home/Feed/CommentModel";
+import { PostPage } from "../../components/Home/PostPage/PostPage";
+export const SingleTweetPage = ()=>{
+    const navigate = useNavigate();
+    const user = JSON.parse(sessionStorage.getItem('AuthToken'));
+    if (!user){
+        navigate('/')
+    }
+    const [navProf,setNavProf] = useRecoilState(ProfileState);
+    const [IsOpen,setIsOpen] = useRecoilState(modelState);
+    const [postpage,setPostpage] = useRecoilState(TweetPageState);
+
+    return (
+        <div className="home">
+
+          <Sidebar/>
+          <PostPage/>
+
+      {IsOpen && <CommentModel/>}
+      <Widgets />
+    </div>
+  );
+};
