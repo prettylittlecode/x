2522bb717839b19adfe14128918e5fb43905edd1
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-24 20:52:08 +1200

    Update Signup.jsx

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -34,7 +34,7 @@ export const Signup = () => {
       <div className="signup-form-container">
         <div className="head-content">
           <div>
-            <Twitter style={{ width: "50px", height: "auto" }} />
+            <img class="ariapluslogo" src="./images/ariaplus.svg" alt="" }} />
           </div>
           <div>
             <h1>Happening Now</h1>
