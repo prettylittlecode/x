aaf6326ab942e92db6c0e60dc9bc57906dfdb8ea
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-24 17:37:23 +1200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import { Profile } from './components/Home/Profile/Profile';
 import { PostPage } from './components/Home/PostPage/PostPage';
 import { ProfilePage } from './pages/profile/profilepage';
 import { SingleTweetPage } from './pages/postpage/SinglePage';
-import { AriaPlusIcon } from "./images/Ariaplus";
+import { AriaPlusIcon } from './images/Ariaplus';
 
 function App() {
 
