c9c1fe4d6e267e603af33ec0564a2e8b656a8415
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-24 18:13:09 +1200

    Update Signup.jsx

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import "./signup.css";
-import { ReactComponent as AriaPlusIcon} from "./assets/ariaplus.svg";
+import { AriaPlusIcon } from "./components/icons/AriaPlusIcon";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { SignupForm } from "../../components/auth/ManualSignup";
