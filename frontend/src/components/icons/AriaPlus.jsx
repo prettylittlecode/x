685c7add5b422861796b826f59d4f5c4dd1730fa
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 00:28:08 +1200

    Update AriaPlus.jsx

--- frontend/src/components/icons/AriaPlus.jsx
+++ frontend/src/components/icons/AriaPlus.jsx
@@ -3,7 +3,7 @@ import { SvgIcon } from '@material-ui/core';
 
 const AriaPlusIcon = (props) => {
   return (
-      <img viewBox="0 0 24 24" src="../images/ariaplus.png" alt="Aria+" />
+      <img viewBox="0 0 24 24" src="./images/ariaplus.png" alt="Aria+" />
   );
 };
 
