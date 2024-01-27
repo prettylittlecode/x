58a53373439498e96a8bbc502ebc3af890fa8454
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:35:15 +1200

    Create Market.jsx

---
+++ frontend/src/components/icons/Market.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const MarketIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/market.svg" alt="" />
+  );
+};
+
+export default MarketIcon;
