e2c2bcebd8339388c12d5ada0233412b847b68aa
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:32:00 +1200

    Create Ai.jsx

---
+++ frontend/src/components/icons/Ai.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const AiIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/ai.svg" alt="" />
+  );
+};
+
+export default AiIcon;
