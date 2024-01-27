e3a5642f18a47a5465f0abe892e18f26a222e778
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:39:13 +1200

    Create Coms.jsx

---
+++ frontend/src/components/icons/Coms.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const ComsIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/coms.svg" alt="" />
+  );
+};
+
+export default ComsIcon;
