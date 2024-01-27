91372e797e329b12b566e3e2cb50f4913d8f78a9
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:08:41 +1200

    Create Explore.jsx

---
+++ frontend/src/components/icons/Explore.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const ExploreIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/explore.svg" alt="" />
+  );
+};
+
+export default ExploreIcon;
