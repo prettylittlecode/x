d9e5aa243aa55961999172af8ee8ffdcd79c8f4d
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:28:52 +1200

    Create Notifications.jsx

---
+++ frontend/src/components/icons/Notifications.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const BellIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/bell.svg" alt="" />
+  );
+};
+
+export default BellIcon;
