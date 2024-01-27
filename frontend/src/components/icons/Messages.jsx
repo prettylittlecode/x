9ecce2f7f19ede8ff72eefc78ac7e6f3b2db9947
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:30:27 +1200

    Create Messages.jsx

---
+++ frontend/src/components/icons/Messages.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const MessagesIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/messages.svg" alt="" />
+  );
+};
+
+export default MessagesIcon;
