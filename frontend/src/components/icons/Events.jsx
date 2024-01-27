024746b42dfbe8e65027d4f319d78f6f1742bcfc
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:40:00 +1200

    Create Events.jsx

---
+++ frontend/src/components/icons/Events.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const EventsIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/events.svg" alt="" />
+  );
+};
+
+export default EventsIcon;
