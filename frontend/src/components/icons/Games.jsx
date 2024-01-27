b04ce2258e957a183739467938458c07708aafa1
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:36:29 +1200

    Create Games.jsx

---
+++ frontend/src/components/icons/Games.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const GameIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/games.svg" alt="" />
+  );
+};
+
+export default GameIcon;
