45a561d7f4d451f0a1b1a3271f01ff71dfe49bb8
prettylittlecode <116849033+prettylittlecode@users.noreply.github.com>
2024-01-27 13:34:28 +1200

    Create Music.jsx

---
+++ frontend/src/components/icons/Music.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+const MusicIcon = (props) => {
+  return (
+      <img class="homeicon" viewBox="0 0 24 24" src="./images/music.svg" alt="" />
+  );
+};
+
+export default MusicIcon;
